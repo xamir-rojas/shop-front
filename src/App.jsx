3419f1574d4577ddf558a4328bc60045755c8f6a
xamir e rojas <xamir.e.rojas@gmail.com>
2021-07-16 18:35:45 -0500

    SignUp cognito integration and MainLayout adaptation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
-import { Signup } from "./pages/Signup";
+import Signup from "./pages/Signup";
 import Base from "./layouts/Base";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
-
 
 export class App extends Component {
   render() {
@@ -13,17 +12,11 @@ export class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Login} />
-          <Route>
-            <MainLayout>
-              <Route exact path="/home" component={Home}/>
-            </MainLayout>
-          </Route>
-          <Route>
-            <Base>
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-            </Base>
-          </Route>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <MainLayout>
+            <Route exact path="/home" component={Home} />
+          </MainLayout>
         </Switch>
       </BrowserRouter>
     );
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,20 +1,52 @@
-import React, { Component } from 'react'
-import '../components/styles/Signup.css';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "../components/styles/Signup.css";
+import { CognitoUserPool } from "amazon-cognito-identity-js";
+import { useHistory } from "react-router-dom";
 
-export class Signup extends Component {
-    render() {
-        return (
-            <>
-                <div className="form__container">
-                    <h1 className="h2 text-blue-600" > Sign up</h1>
-                    <input type="text" className="input" placeholder="Username" />
-                    <input type="password" className="input" placeholder="password" />
-                    <Link to="/home" className="btn--primary" > Sign Up</Link>
-                </div>
-            </>
-        )
-    }
-}
+export default Signup => {
+  let history = useHistory();
+  const poolData = {
+    ClientId: "3unsnaq2372t4pi95h8kle1dhf",
+    UserPoolId: "us-east-2_EwgRcRgUs",
+  };
+    // Password1!
+  const userPool = new CognitoUserPool(poolData);
 
-export default Signup;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    userPool.signUp(email, password, [], null, (err, data) => {
+      if (err) console.error(err);
+      console.log(data);
+    });
+    history.push("/home");
+  };
+
+  return (
+    <>
+      <form className="form__container" onSubmit={handleSubmit}>
+        <h1 className="h2 text-blue-600"> Sign up</h1>
+        <input
+          type="text"
+          className="input"
+          placeholder="Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          type="password"
+          className="input"
+          placeholder="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit" className="btn--primary">
+          Sign Up
+        </button>
+      </form>
+    </>
+  );
+};
