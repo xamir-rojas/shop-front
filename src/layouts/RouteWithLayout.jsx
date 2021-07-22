e43852e75dd4655f17066b7be87533a3d8575a23
xamir e rojas <xamir.e.rojas@gmail.com>
2021-07-22 17:49:26 -0500

    Commentaries

--- src/layouts/RouteWithLayout.jsx
+++ src/layouts/RouteWithLayout.jsx
@@ -7,7 +7,14 @@ import { Redirect } from "react-router";
 import { AccountContext } from "../Account";
 
 const RouteWithLayout = (props) => {
+  /*
+  Component that check the route and the status of the user's session
+  If user is logged then renders a Layout and a Component (usually a Page)
+  Else just redirect to the Login page
+  */
+  // ...rest takes the rest of the props and groups them in ...rest 
   const { layout: Layout, component: Component, ...rest } = props;
+  
   const { getSession } = useContext(AccountContext);
   const [status, setStatus] = useState(false);
   useEffect(() => {
